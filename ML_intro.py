42c47e9ffe7b6ee6680b65c9eba4a7c416e65ba8
Jun Liu <jun.liu15@wdc.com>
2020-10-23 22:07:30 -0700

    adding starting project

---
+++ ML_intro.py
@@ -0,0 +1,12 @@
+#1. Import the data
+#2. Clean the data
+#3. Split the data. Training Set, Input/Test Set, Output
+#4. Create a model. Import algorithm from a library
+#5. Check the output
+#6. Improve
+
+#numpy, pandas, scikit-learn, matplotlib, Jupiter notebook, kaggle
+#kaggle: dataset source
+#Jupiter notebook: python interface
+#panda: data analysis
+
